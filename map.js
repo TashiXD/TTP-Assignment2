339d96590c21914af5e01a038e99cb4d8db837c5
Tashi <112285214+Tashi23@users.noreply.github.com>
2023-06-13 21:18:36 -0400

    changed function name to myMap

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 const arr = [1,2,3];
 
-function map (arr, callBack){
+function myMap (arr, callBack){
     const newArr =[];
     for(let i = 0; i < arr.length; i++){
         callBack(newArr,arr[i], i);
@@ -12,7 +12,7 @@ function square(newArr, value, index){
     return newArr[index] = value*value;
 }
 
-const modded = map(arr, square);
+const modded = myMap(arr, square);
 
 console.log(`Before: ${arr}`);
 console.log(`New Arr: ${modded}`);
